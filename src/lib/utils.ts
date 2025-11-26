f67a7d7e347d619f97109939400d191aead72401
Jeremiah Lena <jeremiahlena13@gmail.com>
2025-11-26 22:28:39 +0100

    CLRY-41 posthog integration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function createOnChangeHandler(onChange: (value: string) => void) {
+  return (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+}
